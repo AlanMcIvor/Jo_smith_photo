2207d316da485405b49c9d364ada769d18bb9e6e
Your Name <you@example.com>
2024-01-01 19:48:21 +0000

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,28 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    screens: {
-      sm: "340px",
-      // => @media (min-width: 640px) { ... }
+    extends: {
 
-      md: "950px",
-      // => @media (min-width: 768px) { ... }
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
 
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
+      screens: {
+        sm: "340px",
+        // => @media (min-width: 640px) { ... }
 
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
+        md: "750px",
+        // => @media (min-width: 768px) { ... }
 
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+      },
     },
   },
   plugins: [],
